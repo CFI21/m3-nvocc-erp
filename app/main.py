338e238de4fb8f20c73654c9f61e115c9cc9282a
CFI21 <cfikabul@gmail.com>
2026-09-25 23:12:33 +0200

    M3 CLX-024 register provider onboarding router

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ from .preprod import router as preprod_router, log_request, get_runtime_mode_val
 from .prodprep import router as prodprep_router
 from .liveprep import router as liveprep_router
 from .live_provider_runtime import router as live_provider_router
+from .provider_onboarding import router as provider_onboarding_router
 
 HERE=Path(__file__).resolve().parent
 META=json.loads((HERE/'module_meta.json').read_text())
@@ -38,6 +39,7 @@ app.add_middleware(
     expose_headers=['X-Request-Id','X-Correlation-Id'],
 )
 app.mount('/static',StaticFiles(directory=HERE/'static'),name='static')
+app.include_router(provider_onboarding_router)
 app.include_router(live_provider_router)
 app.include_router(liveprep_router)
 app.include_router(prodprep_router)
